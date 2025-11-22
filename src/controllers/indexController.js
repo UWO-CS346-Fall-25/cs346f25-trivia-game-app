20f851cd197b2f3071a5ad59dfe0d500a00e0c6a
Quinton Phalin <phalinq@gmail.com>
2025-11-21 21:02:16 -0600

    Updated IndexController to include the trivia API and initialize questions

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -23,6 +23,71 @@ const supabase = require('../supabase');
  */
 exports.getHome = async (req, res, next) => {
   try {
+    // If there's nothing in the databse, populate it with default questions using the API
+    const { count } = await supabase
+      .from("games")
+      .select("id",
+        { head: true, count: "exact" }
+      );
+
+    if (count === 0) {
+      const response = await fetch("https://opentdb.com/api.php?amount=48&type=multiple");
+
+      const data = await response.json();
+      const questions = data.results;
+
+      console.log("Is array:", Array.isArray(questions));
+      console.log("Length:", questions.length);
+
+      for (let i = 0; i < 8; i++) {
+        const title = "Game " + (i + 1);
+        const slug = "game-" + (i + 1);
+        const description = "Default Game";
+
+        const { data, error: sqlError } = await supabase
+          .from("games")
+          .insert([
+            { title: title, description: description, slug: slug },
+          ])
+          .select();
+
+        const id = data[0].id;
+
+        for (let j = 0; j < 6; j++) {
+          const currQuestion = questions.shift();
+          let answers = currQuestion.incorrect_answers.concat(currQuestion.correct_answer);
+          shuffle(answers);
+
+          for (let i = 0; i < answers.length; i++) {
+            answers[i] = decodeHtml(answers[i]);
+          }
+
+          //Decode/format the provided answers/question
+          const questionText = decodeHtml(currQuestion.question);
+          const correct = decodeHtml(currQuestion.correct_answer);
+
+          //Find which letter the answer is at
+          const index = answers.indexOf(correct);
+          const letters = ['A', 'B', 'C', 'D'];
+          let correctLetter = letters[index];
+
+          const { data, error } = await supabase
+            .from("questions")
+            .insert([{
+              game_id: id, question: questionText,
+              option_a: answers[0], option_b: answers[1],
+              option_c: answers[2], option_d: answers[3], answer: correctLetter
+            },])
+            .select();
+        }
+
+        if (sqlError) {
+          console.log("database error");
+        }
+      }
+    }
+
+
     const { data, error } = await supabase
       .from('games')
       .select('id, title, description, slug')
@@ -42,6 +107,27 @@ exports.getHome = async (req, res, next) => {
     next(error);
   }
 }
+
+/**
+ * Helper method to shuffle the answers in an array
+ */
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+//Helper to fix the returned JSON strings
+function decodeHtml(str) {
+  return str
+    .replaceAll('&quot;', '"')
+    .replaceAll('&#039;', "'")
+    .replaceAll('&amp;', '&')
+    .replaceAll('&lt;', '<')
+    .replaceAll('&gt;', '>');
+}
+
 
 /**
  * GET /about
